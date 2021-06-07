e689fbe1f0435aade1a59ab794b56a7f338c839e
MohammadRezahosseinian <mohammadrezahsn80@gmail.com>
2021-06-07 20:24:16 +0430

    add state for gameplay

---
+++ MafiaServer/src/gameplay/GameState.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gameplay;
+
+/**
+ *
+ * @author mohammadreza
+ */
+public enum GameState {
+	DAY_PHASE,
+	VOTE_AFTER_DAY,
+	MAYOR_ACT,
+	DOCTOR_LECTER_ACT,
+	MAFIA_TEAM_NIGHT_ACT,
+	DOCTOR_ACT,
+	DETECTIVE_ACT,
+	PROFESSIONAL_ACT,
+	PSYCO_ACT,
+	DIE_HARD_ACT,
+	READY_PENDING,
+	INTER_STATES_PHASE
+}
