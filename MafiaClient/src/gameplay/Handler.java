70a170496e20f453d20a983d1fc6699abc3d93a7
MohammadRezahosseinian <mohammadrezahsn80@gmail.com>
2021-06-12 22:50:52 +0430

    overide run method in Handler.java in client side handler

--- MafiaClient/src/gameplay/Handler.java
+++ MafiaClient/src/gameplay/Handler.java
@@ -7,24 +7,54 @@ package gameplay;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author mohammadreza
  */
-public class Handler {
+public class Handler implements Runnable{
 
 	private Player player;
-	private Socket connection;
 	private DataInputStream input;
 	private DataOutputStream out;
-	private String username;
-
+	private final String username;
+	private GameState currentState;
+	
 	public Handler(String host, int port, String username) {
 		Config config = Config.createConnection(host, port, username);
-		this.connection = config.getConnection();
+		Socket connection = config.getConnection();
+		this.currentState = GameState.SHOW_RECEIVED_MESSAGE_STATE;
+		
+		try {
+			this.out = (DataOutputStream) connection.getOutputStream();
+			this.input = (DataInputStream) connection.getInputStream();
+		} catch (IOException ex) {
+			Logger.getLogger(Handler.class.getName()).log(Level.SEVERE, null, ex);
+		}
 		this.username = config.getUsername();
+	}
+
+	@Override
+	public void run() {
+		//
+		ClientCommandHandler cch = new ClientCommandHandler(this.out, this);
+		ClientReceivedDataHandler crdh = new ClientReceivedDataHandler(this.input, this);
+		new Thread(cch).start();
+		new Thread(crdh).start();
+		while(true){}
+		//
+	}
+	
+	public GameState getGameState(){
+		return this.currentState;
+	}
+	
+	public synchronized void setGameState(GameState s){
+		this.currentState = s;
 	}
 
 }
