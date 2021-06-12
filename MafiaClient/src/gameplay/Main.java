6a9fd41412ef299e869ccff6abac9ff42cfc97fe
MohammadRezahosseinian <mohammadrezahsn80@gmail.com>
2021-06-12 22:52:42 +0430

    handler multi thread programing in main class

--- MafiaClient/src/gameplay/Main.java
+++ MafiaClient/src/gameplay/Main.java
@@ -1,12 +1,6 @@
 package gameplay;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.Socket;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -25,24 +19,10 @@ public class Main {
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
-		try {
-			// TODO code application logic here
-			Socket connection = new Socket("127.0.0.1",8080);
-			System.out.println("[+] connect to server!");
-			DataOutputStream dos = new DataOutputStream(connection.getOutputStream());
-			DataInputStream dis = new DataInputStream(connection.getInputStream());
-			String cmd ;
-			Scanner input = new Scanner(System.in);
-			while(true){
-				System.out.println("[+] please insert your cmd:");
-				cmd = input.next();
-				dos.writeUTF(cmd);
-				dos.flush();
-//				System.out.println(dis.readUTF());
-			}
-		} catch (IOException ex) {
-			Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-		}
+		Scanner inputScanner = new Scanner(System.in);
+		String username = inputScanner.next();
+		Handler clientHandler = new Handler(Constant.SERVER_HOST, Constant.SERVER_PORT, username);
+		new Thread(clientHandler).start();
 	}
 	
 }
