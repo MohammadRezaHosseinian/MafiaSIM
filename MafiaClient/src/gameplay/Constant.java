e77062a27ce9ceeecc94d186607a516ef786d04a
MohammadRezahosseinian <mohammadrezahsn80@gmail.com>
2021-06-12 22:51:48 +0430

    write some constant variable

--- MafiaClient/src/gameplay/Constant.java
+++ MafiaClient/src/gameplay/Constant.java
@@ -15,5 +15,7 @@ public class Constant {
 	public static final String ROUTE_LIST_ROOMS = "room-list";
 	public static final String ROUTE_CREATE_ROOM = "create-room";
 	public static final String ROUTE_JOIN_ROOM = "join-room";
+	public static final String SERVER_HOST = "127.0.0.1";
+	public static final int SERVER_PORT = 8080;
 
 }
