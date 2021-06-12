2d5859a952c9e03a4c88c3cfbb7f251b645d491d
MohammadRezahosseinian <mohammadrezahsn80@gmail.com>
2021-06-12 22:40:45 +0430

    [+] create state for indicate game state

---
+++ MafiaClient/src/gameplay/GameState.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gameplay;
+
+/**
+ *
+ * @author mohammadreza
+ */
+public enum GameState {
+	SHOW_MENU_STATE,
+	SHOW_RECEIVED_MESSAGE_STATE
+}
