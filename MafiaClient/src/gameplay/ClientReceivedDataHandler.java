13eb47c646ac61a096c1e734335898e2f287db87
MohammadRezahosseinian <mohammadrezahsn80@gmail.com>
2021-06-12 22:42:54 +0430

    [+] create runnable class for handelr recived data from server

---
+++ MafiaClient/src/gameplay/ClientReceivedDataHandler.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gameplay;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author mohammadreza
+ */
+public class ClientReceivedDataHandler implements Runnable {
+
+	private final DataInputStream dis;
+	private final Handler handler;
+
+	public ClientReceivedDataHandler(DataInputStream dis, Handler handler) {
+		this.dis = dis;
+		this.handler = handler;
+	}
+
+	@Override
+	public void run() {
+		String receivedData;
+		while (true) {
+			if (this.handler.getGameState().equals(GameState.SHOW_MENU_STATE)) {
+				try {
+					Thread.sleep(150);
+				} catch (InterruptedException ex) {
+					Logger.getLogger(ClientReceivedDataHandler.class.getName()).log(Level.SEVERE, null, ex);
+				}
+				continue;
+			}
+			try {
+				receivedData = this.dis.readUTF();
+				System.out.println(receivedData);
+				this.handler.setGameState(GameState.SHOW_MENU_STATE);
+			} catch (IOException ex) {
+				Logger.getLogger(ClientReceivedDataHandler.class.getName()).log(Level.SEVERE, null, ex);
+			}
+		}
+	}
+
+}
