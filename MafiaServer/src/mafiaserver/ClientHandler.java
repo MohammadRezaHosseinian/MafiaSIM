b0c4302471b97c3af5bfdafae86b2c308b6a8ff1
MohammadRezahosseinian <mohammadrezahsn80@gmail.com>
2021-06-14 18:52:17 +0430

    completed client handler

--- MafiaServer/src/mafiaserver/ClientHandler.java
+++ MafiaServer/src/mafiaserver/ClientHandler.java
@@ -6,11 +6,11 @@
 package mafiaserver;
 
 import gameplay.Room;
+import gameplay.RoomHandler;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -23,26 +23,17 @@ public class ClientHandler implements Runnable {
 	private final Socket connection;
 	private DataOutputStream dos;
 	private DataInputStream dis;
-	private ArrayList<Room> rooms;
+	private RoomHandler roomHandler;
 
-	public ClientHandler(Socket connection) {
+	public ClientHandler(Socket connection, RoomHandler roomHandler) {
 		this.connection = connection;
-		this.rooms = new ArrayList<>();
+		this.roomHandler = roomHandler;
 		try {
 			dos = new DataOutputStream(this.connection.getOutputStream());
 			dis = new DataInputStream(this.connection.getInputStream());
 		} catch (IOException ex) {
 			Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
 		}
-	}
-
-	private Room getRoom(String roomName) {
-		for (Room room : rooms) {
-			if (room.getName().equals(roomName)) {
-				return room;
-			}
-		}
-		return null;
 	}
 
 	@Override
@@ -76,7 +67,7 @@ public class ClientHandler implements Runnable {
 				}
 				break;
 			case 2:
-				if(spliteReq[0].equals(Constants.ROUTE_LIST_ROOM_USERS)){
+				if (spliteReq[0].equals(Constants.ROUTE_LIST_ROOM_USERS)) {
 					String roomName = spliteReq[1];
 					this.roomUsersListCmd(roomName);
 				}
@@ -84,10 +75,29 @@ public class ClientHandler implements Runnable {
 			case 3:
 				if (spliteReq[0].equals(Constants.ROUTE_CREATE_ROOM)) {
 					this.createRoomCmd(spliteReq[1], spliteReq[2]);
+				} else if (spliteReq[0].equals(Constants.ROUTE_JOIN_ROOM)) {
+					roomname = spliteReq[1];
+					Room room = this.roomHandler.getRoomByName(roomname);
+					if (room != null) {
+						room.handleReq(this.dos, spliteReq[0], spliteReq[2]);
+					}
+					else{
+						System.out.println("no room with name: "+spliteReq[1]);
+						this.roomHandler.showRooms();
+					}
 				} else {
 					roomname = spliteReq[0];
-					Room room = this.getRoom(roomname);
-					room.handleReq(this.dos, spliteReq[1], spliteReq[2]);
+					Room room = this.roomHandler.getRoomByName(roomname);
+					if (room != null) {
+						room.handleReq(this.dos, spliteReq[1], spliteReq[2]);
+					}
+				}
+				break;
+			case 4:
+				roomname = spliteReq[0];
+				Room room = this.roomHandler.getRoomByName(roomname);
+				if (room != null) {
+					room.handleReq(spliteReq[1], spliteReq[2], spliteReq[3]);
 				}
 				break;
 			default:
@@ -98,24 +108,26 @@ public class ClientHandler implements Runnable {
 
 	private void createRoomCmd(String roomname, String roomSize) {
 		System.out.println("Create Room : " + roomname);
-		int playersCount;
+		int playersCount = 10;
 		try {
 			playersCount = Integer.parseInt(roomSize);
+			this.dos.writeUTF("ok");
 		} catch (NumberFormatException e) {
 			System.out.format("[-] Oops , can't parse %s as int", roomSize);
 			return;
+		} catch (IOException ex) {
+			Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
 		}
 
-		Room room = new Room(roomname, playersCount);
-		this.rooms.add(room);
-		new Thread(room).start();
+		this.roomHandler.addRoom(roomname, playersCount);
+		new Thread(this.roomHandler.getRoomByName(roomname)).start();
 
 	}
 
 	private void roomListCmd() {
 		try {
 			System.out.println("[+] rooms list cmd func: ");
-			this.dos.writeUTF(Utils.listRooms(this.rooms));
+			this.dos.writeUTF(Utils.listRooms(this.roomHandler.getRooms()));
 		} catch (IOException ex) {
 			Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
 		}
@@ -125,15 +137,15 @@ public class ClientHandler implements Runnable {
 		System.out.println("[+] users list cmd func: ");
 
 		try {
-			this.dos.writeUTF(Utils.listAllUser(this.rooms));
+			this.dos.writeUTF(Utils.listAllUser(this.roomHandler.getRooms()));
 		} catch (IOException ex) {
 			Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
 		}
 	}
 
 	private void roomUsersListCmd(String roomName) {
-		Room room = this.getRoom(roomName);
-		if(room != null){
+		Room room = this.roomHandler.getRoomByName(roomName);
+		if (room != null) {
 			try {
 				this.dos.writeUTF(Utils.listRoomUser(room));
 			} catch (IOException ex) {
@@ -142,11 +154,11 @@ public class ClientHandler implements Runnable {
 			return;
 		}
 		try {
-			this.dos.writeUTF(String.format(Constants.MSG_BAD_ROOM_NAME_ERRORE,roomName,Constants.ROUTE_LIST_ROOMS));
+			this.dos.writeUTF(String.format(Constants.MSG_BAD_ROOM_NAME_ERRORE, roomName, Constants.ROUTE_LIST_ROOMS));
 		} catch (IOException ex) {
 			Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
 		}
-		
+
 	}
 
 }
